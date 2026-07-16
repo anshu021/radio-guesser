66426d8dbfe024f2a0f262b8425595fe0548cace
anshu021 <divyanshu021j@gmail.com>
2026-07-16 14:51:04 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/divyanshu/radioguesser/RadioGuesserApplication.java
@@ -0,0 +1,13 @@
+package com.divyanshu.radioguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioGuesserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioGuesserApplication.class, args);
+	}
+
+}
